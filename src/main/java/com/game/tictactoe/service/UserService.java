46a1643075d580ee4a9f3aa50e5ade27dc6c138c
Mera <meragebreyes@gmail.com>
2024-01-10 22:50:14 +0100

    UserServiceTest

--- src/main/java/com/game/tictactoe/service/UserService.java
+++ src/main/java/com/game/tictactoe/service/UserService.java
@@ -75,4 +75,8 @@ public class UserService {
         );
     }
 
+    public Object getUser() {
+        return null;
+    }
+
 }
---
+++ src/test/java/com/game/tictactoe/UserServiceTest.java
@@ -0,0 +1,41 @@
+package com.game.tictactoe;
+
+import com.game.tictactoe.service.UserService;
+import com.game.tictactoe.web.api.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrlTemplate;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserService.class)
+        public class UserServiceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void testGetRoute() throws Exception {
+       //Test Daten und Service Mock
+       User user = new User(1,"username", "password", "points");
+       user.setId(1);
+       when(userService.getUser()).thenReturn(user);
+       //Erwartetes Ergebnis
+         String expected = "{\"id\":1,\"username\":\"username\",\"password\":\"password\",\"points\":\"points\"}";
+         //Aufruf und Vergleich
+        this.mockMvc.perform(get("user"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(expected));
+    }
+}
+}
